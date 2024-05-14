588ef18934f59fd6ca42c3b93a4e776f38962a1f
Raluca Rusu <rr.ralucarusu@gmail.com>
2024-05-14 23:57:01 +0300

    updated files

--- main.py
+++ main.py
@@ -32,7 +32,26 @@ def get_similarity():
     if not file1 or not file2:
         return jsonify({"invalid_request_error": "Invalid files provided."}), 400
 
-    similarity = 1234 # SimilarityHandler.get_response(model, file1, file2)
+    def load_image_from_file(file, transformation):
+                image = Image.open(file).convert('RGB')
+                if transformation is not None:
+                    image = transformation(image)
+                    image = image.unsqueeze(0)
+                return image
+
+    transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                ])
+
+    image1 = load_image_from_file(file1, transform)
+    image2 = load_image_from_file(file2, transform)
+
+    #with torch.no_grad():
+    #    features1 = model(image1, return_embeddings=True)
+    #    features2 = model(image2, return_embeddings=True)
+    #similarity = functional.cosine_similarity(features1, features2)
+    similarity = 123
 
     if similarity:
         return jsonify({"similarity": round(similarity, 2)})
